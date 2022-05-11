a363e4fc11e7d7b3f836f70b028358826328072e
mariathar <tharegorodceva.maria@mail.ru>
2022-05-11 18:06:29 +0200

    fix build

--- adder_test.go
+++ adder_test.go
@@ -6,6 +6,6 @@ func TestAdded(t *testing.T) {
 	sum := Add(2, 2)
 	expected := 4
 	if sum != expected {
-		t.Errorf("expected '$d' but got '$d'", expected, sum)
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
 	}
 }
